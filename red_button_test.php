4fc4627e968dca81a08bc97feabe94a3b9a37620
Vladimir <vovacomua@bk.ru>
2016-10-10 22:28:43 +0300

    Add button to frontend

--- red_button_test.php
+++ red_button_test.php
@@ -12,4 +12,26 @@ Version: 0.1
 Author URI: http://vldmr.xyz
 */
 
+ 
+ function add_red_buton( ) {
+    global $post;
+	global $hook_flag2;
+	$hook_flag2 += 1;
+	
+	if ($hook_flag2 == 2 && $post->ID){
+		 $the_form = '
+		 <div class="comments-area">
+		 <form id="theForm">
+		 <input id="name" name="name" value = "'.$post->ID.'" type="hidden" />
+		 <input name="action" type="hidden" value="the_ajax_hook" />&nbsp; <!-- this puts the action the_ajax_hook into the serialized form -->
+		 <input id="submit_button" value = "Click This" type="button" onClick="submit_me();" />
+		 </form>
+		 <div id="response_area">
+		 This is where we\'ll get the response
+		 </div> </div>';
+	echo $the_form;
+	}
+}
+add_action( "pre_get_comments", "add_red_buton" );
+
 ?>
