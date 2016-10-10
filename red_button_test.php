6a1316dde9622ef354957d42ab4104bf432f0b3e
Vladimir <vovacomua@bk.ru>
2016-10-10 22:17:29 +0300

    Initial commit

---
+++ red_button_test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package red_button_test
+ * @version 0.1
+ */
+/*
+Plugin Name: Red Button Test
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: This is test assignment
+Author: Vladimir Revenko
+Version: 0.1
+Author URI: http://vldmr.xyz
+*/
+
+?>
