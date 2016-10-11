263e764b18bafc9c005a9c7801cbc46daea6d3cf
Vladimir <vovacomua@bk.ru>
2016-10-11 11:57:05 +0300

    JS error handler

--- ajax.js
+++ ajax.js
@@ -1,13 +1,16 @@
 function submit_me(){
 var dt = new Date();
 var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-
 jQuery("#theForm").append('<input type="hidden" name="time" value="'+time+'" /> ');
+
 jQuery.post(the_ajax_script.ajaxurl, jQuery("#theForm").serialize()
 ,
 function(response_from_the_action_function){
 jQuery("#response_area").html(response_from_the_action_function);
 jQuery("#submit_button").prop('disabled', true);
 }
-);
+)
+.fail(function(response) {
+	jQuery("#response_area").html('Error: ' + response.responseText);
+});
 }
