34a947be5bf64cc188da1040e7905a0f177a0d2c
Alexander <ac@webcodist.com>
2016-10-17 13:45:56 +0300

    Edit code style

--- red-button-test.php
+++ red-button-test.php
@@ -24,14 +24,14 @@ add_action( 'wp_ajax_the_ajax_hook', 'the_action_function' );
 add_action( 'wp_ajax_nopriv_the_ajax_hook', 'the_action_function' ); // need this to serve non logged in users
 
 add_action( 'wp_enqueue_scripts', 'myajax_data', 99 );
-function myajax_data(){
+
+function myajax_data() {
 	wp_enqueue_style( 'red-button-test', plugin_dir_url( __FILE__ ) . 'red-button-test.css',false,'1.1','all' );
 	wp_enqueue_script( 'my-ajax-handle', plugin_dir_url( __FILE__ ) . 'ajax.js', array( 'jquery' ) );
 	wp_localize_script( 'my-ajax-handle', 'the_ajax_script', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) , 'nonce' => wp_create_nonce('myajax-nonce') ) );
 }
  
-function custom_meta_box_markup2()
-{
+function custom_meta_box_markup2() {
 	global $post;
  
 	$post_id = $post->ID;
@@ -46,20 +46,19 @@ function custom_meta_box_markup2()
 	//}
 
 	foreach ($stored_clicks as $stored_click) {
-		echo '<p>'. esc_attr($stored_click["time"]) .' :: '. esc_attr($stored_click["client_IP"]) . '</p>' ;
+		echo '<span> <b>'. esc_attr($stored_click["time"]) .'</b> :: '. esc_attr($stored_click["client_IP"]) . ' </span>' ;
 	}
 }
 
-function add_custom_meta_box2()
-{
+function add_custom_meta_box2() {
     add_meta_box( "demo-meta-box2", "Red Button Clicks 2", "custom_meta_box_markup2", "post", "side", "high", null );
 }
 
 add_action( "add_meta_boxes", "add_custom_meta_box2" );
  
  // THE FUNCTION
-function the_action_function(){
-	if (empty( $_POST['nonce'] ) || empty( $_POST['post-id'] ) || empty( $_POST['time'] )) {
+function the_action_function() {
+	if ( empty( $_POST['nonce'] ) || empty( $_POST['post-id'] ) || empty( $_POST['time'] ) ) {
 		die ( 'Error!');
 	}
 
@@ -70,13 +69,13 @@ function the_action_function(){
 	}
 
 	$client_post_ID = sanitize_text_field( $_POST['post-id'] );
-	$time = sanitize_text_field( $_POST['time']);
+	$time = sanitize_text_field( $_POST['time'] );
 	$client_IP = $_SERVER['REMOTE_ADDR'];
 	//$client_record = $time."::".$client_IP;
 
 	$client_info = array(
 		'time' => $time,
-		'client_IP' => $client_IP
+		'client_IP' => $client_IP,
 	);
 
 	$result = add_post_meta( $client_post_ID, 'red_button3', $client_info );
@@ -88,7 +87,7 @@ function the_action_function(){
 
 	$return = array(
 		'time'	=> $server_time,
-		'ip'	=> $server_IP
+		'ip'	=> $server_IP,
 	);
 
 	wp_send_json_success($return);
@@ -116,6 +115,5 @@ function add_red_button( $content ) {
 	}
 	return $content;
 }
+
 add_filter('the_content', 'add_red_button');
-
-
