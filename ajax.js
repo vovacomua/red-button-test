2d2cdb773695f0049b83c2d8830417495cd04519
Alexander <ac@webcodist.com>
2016-10-18 13:30:42 +0300

    Remove JS commented code

--- ajax.js
+++ ajax.js
@@ -1,18 +1,4 @@
 function submit_me(){
-	// var dt = new Date();
-	// var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-	// //jQuery("#theForm").append('<input type="hidden" name="time" value="'+time+'" /> ');
-    //
-	// var action  = jQuery('#theForm').find('input[name="action"]').val() ;
-	// var nonce   = jQuery('#theForm').find('input[name="nonce"]').val() ;
-    //
-	// //alert( action );
-    //
-	// var data = {
-	// 	'action' : action,
-	// 	'nonce'  : nonce,
-	// 	'time'   : time
-	// };
 
 	var $form = jQuery('#theForm'),
 		dt = new Date(),
